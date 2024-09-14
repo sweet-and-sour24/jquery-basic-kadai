d97a2e05d5a146ac08d8a6c65abbb6497e83c8cb
”sweet-and-sour24” <sweet.and.sour24@icloud.com>
2024-09-14 09:36:18 +0900

    再提出

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,8 +1,6 @@
-$(function() {
-  $(window).on('load', function(){
-    console.log('loadイベントが発生しました');
-  });
-  $(window).on('scroll', function(){
-    console.log('scrollイベントが発生しました');
-  });
+$(window).on('load', () => {
+  console.log('loadイベントが発生しました');
+});
+$(window).on('scroll', ()=> {
+  console.log('scrollイベントが発生しました');
 });
